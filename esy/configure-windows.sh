abadf6a70539a69e2583ec9d7a8d8256c0db4682
Bryan Phelps <bryphe@outlook.com>
2020-02-24 13:18:09 -0800

    More tweaks

--- esy/configure-windows.sh
+++ esy/configure-windows.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/bash
 
-TARGET=$(cygpath -u ${cur__target_dir})
+set -e
 
-echo "Target dir: $TARGET"
-        
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$TARGET -DCMAKE_C_COMPILER=/usr/bin/x86_64-w64-mingw32-gcc -DCMAKE_C_LINK_EXECUTABLE=/usr/bin/x86_64-w64-mingw32-ld -DCMAKE_RC_COMPILER=/usr/bin/x86_64-w64-mingw32-windres.exe -DWIN32=YES -DUNIX=NO .
+sh autogen.sh
+./configure --host=x86_64-w64-mingw32 CC=x86_64-w64-mingw32-gcc PREFIX=$cur__install
+make
+make check
+make install
